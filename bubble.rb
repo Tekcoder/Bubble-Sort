f819ab9ed0726557e1b79af94c78837c7018af69
Audrey-Ella-xo <anitaudrey@gmail.com>
2019-11-04 19:05:17 +0100

    fixed lint errors

--- bubble.rb
+++ bubble.rb
@@ -1,15 +1,17 @@
+# frozen_string_literal: true
+
 def bubble_sort(arr)
   checker = true
-    while checker 
-      checker =  false
-      (arr.length - 1).times do |x|
-      if arr[x] > arr[x + 1] 
+  while checker
+    checker = false
+    (arr.length - 1).times do |x|
+      if arr[x] > arr[x + 1]
         arr[x], arr[x + 1] = arr[x + 1], arr[x]
         checker = true
       end
     end
-    end
+  end
   arr
 end
 
-puts bubble_sort([3,1,2,4,4,7,9])
+puts bubble_sort([3, 1, 2, 4, 4, 7, 9])

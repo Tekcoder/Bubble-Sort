59515d78c42b8df586fc25c8fdc08db5d7fcfcb2
Audrey-Ella-xo <anitaudrey@gmail.com>
2019-11-04 18:23:49 +0100

    removed lint errors

--- bubble.rb
+++ bubble.rb
@@ -1,15 +1,15 @@
 def bubble_sort(arr)
-    checker = true
-    while checker do 
-         checker =  false
-        (arr.length - 1).times do |x|
-        if arr[x] > arr[x+1] 
-            arr[x], arr[x+1] = arr[x+1], arr[x]
-            checker = true
-        end
+  checker = true
+    while checker 
+      checker =  false
+      (arr.length - 1).times do |x|
+      if arr[x] > arr[x + 1] 
+        arr[x], arr[x + 1] = arr[x + 1], arr[x]
+        checker = true
+      end
     end
     end
-    arr
+  arr
 end
 
 puts bubble_sort([3,1,2,4,4,7,9])
